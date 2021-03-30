d757a73d909362bd61bfc76f2f6c4fd2e66d745c
Sai Saranya Lanka <31591022+saisaranyalanka@users.noreply.github.com>
2021-03-30 20:43:40 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -3,12 +3,12 @@ echo "Let's play a game. Give it a try. Guess how many files do we have in hthe 
 Game () {
 if [ $count -eq $actualcount ]
   then 
-      echo "Wow! Congratulations.. Your guess is correct. Have a nice day"
+      echo "Wow! Congratulations.. Your guess is correct"
 elif [ $count -gt $actualcount ]
   then
       echo "the number you guessed is high. It should be less than the number you guessed. Let's give it a try again!!"
 else 
-      echo "the number you guessed is low. It should be greater than the number you guessed. Let's give it a try again!
+      echo "the number you guessed is low. It should be greater than the number you guessed. Let's give it a try again!"
 fi
 }
 while [ $count -ne $actualcount ]
@@ -17,4 +17,6 @@ do
   actualcount=$(ls | wc -l)
   Game
 done  
+echo "have a nice day"
+
   
